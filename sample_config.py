946e73cc49fb4c26a6d4028cb86047eabcebfd9e
Venuja Thilakarathna <91406478+VenujaBots@users.noreply.github.com>
2021-11-06 17:47:17 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "2105578077:AAHIQKleAfQKq4Z55nEyT26ntYlEe9sycyQ")
 
     APP_ID = int(os.environ.get("APP_ID", 12345))
 
